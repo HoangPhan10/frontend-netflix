dfa2365f30c59ba33187a0ba8f8ea98e5ebdf93f
PhanHoang10 <phanhoang1022002@gmail.com>
2022-06-19 18:01:41 +0700

    hoanghoang

--- src/api/users.js
+++ src/api/users.js
@@ -3,7 +3,7 @@ import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 export const usersApi =createApi({
     reducerPath:"users",
     baseQuery:fetchBaseQuery({
-        baseUrl:"http://localhost:4040",
+        baseUrl:"https://backend-netflix.herokuapp.com",
     }),
     endpoints:(builder)=>({
         getUsers:builder.query({
